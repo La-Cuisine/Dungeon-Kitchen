b63ecb5f8e7dd3908d51958d109216b46fb81106
Pavitran <pavsam2310@gmail.com>
2026-06-05 19:56:36 +0200

    init branch - obj: map class

---
+++ obj/map.py
@@ -0,0 +1,65 @@
+class cell:
+  def __init__(self, Content, State, Walls):
+    self.content = Content
+    self.state = State
+    self.walls = Walls
+
+class map: 
+  def __init__(self, X, Y):
+    self._name = "Untitled"
+    self._length = X
+    self._width = Y
+    self._grid = []
+    #map initialisation
+    for i in range(X):
+      l = []
+      for j in range(Y):
+        l.append(0)
+      self._grid.append(l)
+  def __init__(self, s, X, Y):
+    self._name = s
+    self._length = X
+    self._width = Y
+    self._grid = []
+    #map initialisation
+    for i in range(X):
+      l = []
+      for j in range(Y):
+        l.append(0)
+      self._grid.append(l)
+  
+  #getters
+  def name():
+    return self._name
+  def length():
+    return self._length
+  def width():
+    return self._width
+  def grid():
+    return self._grid
+
+  #setters
+  def rename(s):
+    self._name = s
+  def set_cell(x,y,content):
+    self._grid[x][y] = content
+
+  #methods
+  def reset(self):
+    for i in range(self._length):
+      for j in range(self._width):
+        self._grid[i][j] = 0
+  
+  def resize(self, X, Y):
+    new = []
+    for i in range(X):
+      l = []
+      for j in range(Y):
+        if(X>self._length and Y>self._width):
+          l.append(0)
+        else:
+          l.append(self._grid[i][j])
+        new.append(l)
+    self._grid = new
+    self._length = X
+    self._width = Y
