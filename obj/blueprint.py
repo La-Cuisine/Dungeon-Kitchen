3556a468e0a517a90d22fbce50f4af484209cc8b
Pavitran <pavsam2310@gmail.com>
2026-06-06 15:53:26 +0200

    update to blueprint data structure

--- obj/blueprint.py
+++ obj/blueprint.py
@@ -1,8 +1,37 @@
 class Cell:
-  def __init__(self, Content, State, Walls):
-    self.content = Content #?: ex: 0=void 1=path 2=object etc
-    self.state = State #?: ex: 0=empty 1=occupied etc
-    self.walls = Walls #?: 0b1010
+  def __init__(self, Content=0, Walls=0b0000, Doors=0b0000):
+    if(Content<0 or Content>3):
+      raise Exception("InvalidArgument")
+    self._content = Content # 0=path; 1=void; 2=filled; 3=transition
+    if(Walls>0b1111 or Walls<0b000):
+      raise Exception("InvalidArgument")
+    self._walls = Walls # 0b1010
+    if(Doors>0b1111 or Doors<0b000):
+      raise Exception("InvalidArgument")
+    self._doors = Doors # 0b1010
+
+  #getters
+  def content(self):
+    return self._content
+  def walls(self):
+    return self._walls
+  def doors(self):
+    return self._doors
+  
+  #setters
+  def set_content(c):
+    if(c<0 or c>3):
+      raise Exception("InvalidArgument")
+    self._content = c
+  def set_walls(w):
+    if(w>0b1111 or w<0b000):
+      raise Exception("InvalidArgument")
+    self._walls = w # 0b1010
+  def set_doors(d):
+    if(d>0b1111 or d<0b000):
+      raise Exception("InvalidArgument")
+    self._doors = d # 0b1010
+
 
 class Blueprint: 
   def __init__(self, X, Y, s="Unlabeled"):
@@ -18,7 +47,7 @@ class Blueprint:
     for i in range(X):
       l = []
       for j in range(Y):
-        l.append(0)
+        l.append(Cell())
       self._grid.append(l)
 
   #getters
@@ -34,21 +63,23 @@ class Blueprint:
   #setters
   def rename(s):
     self._name = s
-  def set_cell(x,y,content):
-    self._grid[x][y] = content
+  def set_cell(x,y,c):
+
+    self._grid[x][y] = c
 
   #methods
   def reset(self):
     for i in range(self._length):
       for j in range(self._width):
-        self._grid[i][j] = 0
+        self._grid[i][j] = Cell()
   
   def resize(self, X, Y):
+    #resize deletes grid
     new = []
     for i in range(X):
       l = []
       for j in range(Y):
-          l.append(0)
+          l.append(Cell())
       new.append(l)
     self._grid = new
     self._length = X
