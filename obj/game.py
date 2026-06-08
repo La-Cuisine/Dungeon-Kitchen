3abb7bf6cb8b8ce371807fd3f753870e2d90a9ae
Pavitran <pavsam2310@gmail.com>
2026-06-08 20:44:33 +0200

    updated game class new attribute handling for pawns

--- obj/game.py
+++ obj/game.py
@@ -1,16 +1,21 @@
 import blueprint 
+import pawns
+import player
 
 class game:
   def __init__(self):
-    #???
-    self.__base_length = 100
-    self.__base_width = 100
-    self._layers = dict() #(layer_name:map)
+    #TODO
+    self.__base_length = 50
+    self.__base_width = 50
+    self._layers = dict()# (layer_name:map)
+    self._pawns = dict()# ((x,y):occupant)
     self._notes = dict()
   
   #getters
   def layers(self):
     return self._layers
+  def pawns(self):
+    return self._pawns
 
   #setters
 
@@ -37,6 +42,33 @@ class game:
   
   def remove_layer(self,s):
     if not(s in self._layers):
-      raise Exception("DoesNotExist")
-    else:
-      del self._layers[s]
+      raise Exception("LayerDoesNotExist")
+    res = self._layers[s] 
+    del self._layers[s]
+    return res
+
+  def add_pawn(self,p,x,y,l):
+    if((x,y) in self._pawns):
+      raise Exception("CellAlreadyOccupied")
+    if(not (l in self._layers)):
+      raise Exception("LayerDoesNotExist")
+    if(x<0 or x>self._layers[l].length() or y<0 or y>self._layers[l].width()):
+      raise Exception("IndexOutOfRange")
+    if((not isinstance(p,NPC)) and (not isinstance(p,PC))):
+      raise Exception("InvalidArgument")
+    self._pawns[(x,y)] = p
+
+  def remove_pawn(self,x,y):
+    if(not((x,y) in self._pawns)):
+      raise Exception("PawnNotFound")
+    res = self._pawns[(x,y)] 
+    del self._pawns[(x,y)]
+    return res
+
+  def move_pawn(self, x1, y1, x2, y2):
+    if(not((x1,y1) in self._pawns)):
+      raise Exception("PawnNotFound")
+    if((x2,y2) in self._pawns):
+      raise Exception("CellAlreadyOccupied")
+    self._pawns[(x2,y2)] = self._pawns[(x1,y1)]
+    del self._pawns[(x1,y1)]
