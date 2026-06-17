fe70a2fc0bad70f056b8bc4fbc883c7eb5bc52a9
Pavitran <pavsam2310@gmail.com>
2026-06-17 15:21:23 +0200

    obj: syntaxic corr.

--- obj/player.py
+++ obj/player.py
@@ -1,28 +1,28 @@
 import pawns
 
 class Player:
-  def __init__(self, Name, ID=0):
-    self._playerID = ID
-    self.__mdp = "1234"
-    self._char = None
-    self._notes = dict()
+    def __init__(self, Name, ID=0):
+        self._playerID = ID
+        self.__mdp = "1234"
+        self._char = None
+        self._notes = dict()
 
-  #getters
-  def ID(self):
-    return self._playerID
-  def character(self):
-    return self._char
-  
-  #setters
-  def reassign(self, ID):
-    self._playerID = ID
-  def newRole(self, sheet):
-    if(not (isinstance(sheet,PC))):
-      raise Exception("InvalidArgument")
-    self._char = sheet
+    #getters
+    def ID(self):
+        return self._playerID
+    def character(self):
+        return self._char
+    
+    #setters
+    def reassign(self, ID):
+        self._playerID = ID
+    def newRole(self, sheet):
+        if(not (isinstance(sheet,PC))):
+          raise Exception("InvalidArgument")
+        self._char = sheet
 
-  #method
-  def check_password(self, mdp):
-    return (self.__mdp == mdp)
-  def _edit_password(self, mdp, new):
-    self.__mdp = new
+    #method
+    def check_password(self, mdp):
+        return (self.__mdp == mdp)
+    def _edit_password(self, mdp, new):
+        self.__mdp = new
